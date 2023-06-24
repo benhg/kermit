6d28fee13e5bb62cef553b5fd2d71c063abcb547
Ben Glick <glick@glick.cloud>
2023-06-24 10:51:46 -0700

    skeleton

---
+++ src/kermit.py
@@ -0,0 +1,21 @@
+"""
+@file kermit.py
+
+@brief This is the entry point to KERMIT
+"""
+
+import argparse
+import logging
+
+from generate_map import main as map_main
+from collect_data import main as collect_main
+
+
+def main():
+	"""
+	The main entry point
+	"""
+	pass
+
+if __name__ == '__main__':
+	main()
